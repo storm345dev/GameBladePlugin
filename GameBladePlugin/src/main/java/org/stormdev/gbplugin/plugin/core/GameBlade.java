17c426d6e9add68cbcbe727c1d94becd936f78b3
storm345 <storm345dev@gmail.com>
2014-06-25 19:20:35 +0100

    Start on the main bukkit class

--- GameBladePlugin/src/main/java/org/stormdev/gbplugin/plugin/core/GameBlade.java
+++ GameBladePlugin/src/main/java/org/stormdev/gbplugin/plugin/core/GameBlade.java
@@ -3,6 +3,7 @@ package org.stormdev.gbplugin.plugin.core;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public class GameBlade extends JavaPlugin {
 	public static CustomLogger logger;
 	public static GameBlade plugin;
@@ -11,13 +12,11 @@ public class GameBlade extends JavaPlugin {
 	public void onEnable(){
 		plugin = this;
 		logger = new CustomLogger(Bukkit.getConsoleSender(), getLogger());
-		
 		logger.info("GameBladePlugin enabled!");
 	}
 	
 	@Override
 	public void onDisable(){
-		
 		logger.info("GameBladePlugin disabled!");
 	}
 }
