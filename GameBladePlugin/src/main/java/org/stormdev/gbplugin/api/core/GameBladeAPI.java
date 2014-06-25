d9f85f94c05d4fe8734f4310295652708592b68e
storm345 <storm345dev@gmail.com>
2014-06-25 20:46:42 +0100

    Added support for the tokens api

--- GameBladePlugin/src/main/java/org/stormdev/gbplugin/api/core/GameBladeAPI.java
+++ GameBladePlugin/src/main/java/org/stormdev/gbplugin/api/core/GameBladeAPI.java
@@ -1,11 +1,18 @@
 package org.stormdev.gbplugin.api.core;
 
+import org.stormdev.gbapi.storm.tokens.Tokens;
+
 public class GameBladeAPI implements org.stormdev.gbapi.core.GameBladeAPI{
 	private static final double VERSION = 0.1;
 	
 	@Override
 	public double getAPIVersionNumber() {
 		return VERSION;
+	}
+
+	@Override
+	public Tokens getTokenHandler() {
+		return org.stormdev.gbplugin.plugin.stormapis.Tokens.getInstance();
 	}
 
 }
