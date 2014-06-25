324a462a3d223cc4ba7ce8635883251fdc685044
storm345 <storm345dev@gmail.com>
2014-06-25 19:50:00 +0100

    Reduce the pom

--- GameBladePlugin/src/main/java/org/stormdev/gbplugin/plugin/core/GameBlade.java
+++ GameBladePlugin/src/main/java/org/stormdev/gbplugin/plugin/core/GameBlade.java
@@ -12,11 +12,29 @@ public class GameBlade extends JavaPlugin {
 	public void onEnable(){
 		plugin = this;
 		logger = new CustomLogger(Bukkit.getConsoleSender(), getLogger());
+		
+		if(!initAPI()){
+			//TODO Error
+		}
+		
 		logger.info("GameBladePlugin enabled!");
 	}
 	
 	@Override
 	public void onDisable(){
 		logger.info("GameBladePlugin disabled!");
+	}
+	
+	private boolean initAPI(){
+		try {
+			Class.forName("org.stormdev.gbapi.core.APIProvider");
+		} catch (ClassNotFoundException e) {
+			logger.info("Bad GameBladePlugin file, doesn't include the API! :( Please try a different plugin file!");
+			return false;
+		}
+		
+		//TODO
+		
+		return true;
 	}
 }
