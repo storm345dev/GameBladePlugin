4974767bfd18b14f8856b545f4f015bf4292d599
storm345 <storm345dev@gmail.com>
2014-07-04 17:35:54 +0100

    Correct text formatting

--- GameBladePlugin/src/main/java/org/stormdev/gbplugin/plugin/cosmetics/CosmeticManager.java
+++ GameBladePlugin/src/main/java/org/stormdev/gbplugin/plugin/cosmetics/CosmeticManager.java
@@ -12,8 +12,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.stormdev.gbapi.cosmetics.Cosmetic;
 import org.stormdev.gbapi.cosmetics.CosmeticType;
+import org.stormdev.gbapi.cosmetics.Currency;
 import org.stormdev.gbapi.cosmetics.Rank;
 import org.stormdev.gbapi.storm.UUIDAPI.PlayerIDFinder;
+import org.stormdev.gbapi.storm.tokens.Tokens.TokenServiceUnavailableException;
 import org.stormdev.gbplugin.cosmetics.cosmetics.hats.HatRegistry;
 import org.stormdev.gbplugin.plugin.core.GameBlade;
 import org.stormdev.gbplugin.plugin.cosmetics.shop.CosmeticShop;
@@ -66,7 +68,7 @@ public class CosmeticManager {
 		}
 	}
 	
-	public void purchaseCosmetic(Player player, Cosmetic c){
+	public void purchaseCosmetic(final Player player, final Cosmetic c){
 		player.sendMessage("debug: "+c.getID());
 		Rank playerRank = Rank.getRank(player);
 		
@@ -74,7 +76,92 @@ public class CosmeticManager {
 			player.sendMessage(ChatColor.RED+"Sorry that item is "+c.minimumRank().getName()+" and higher only!");
 			return;
 		}
-		//TODO
+		player.sendMessage(ChatColor.GRAY+"Purchasing...");
+		Bukkit.getScheduler().runTaskAsynchronously(GameBlade.plugin, new Runnable(){
+
+			@Override
+			public void run() {
+				List<String> owned = getOwnedCosmeticIds(player);
+				if(owned.contains(c.getID())){
+					player.sendMessage(ChatColor.RED+"You already own this item!");
+					return;
+				}
+				if(c.getPrice() > 0){
+					Currency currency = c.getCurrencyUsed();
+					if(currency.equals(Currency.STARS)){
+						int bal = GameBlade.api.getStarsHandler().getStars(player);
+						if(bal < c.getPrice()){
+							player.sendMessage(ChatColor.RED+"You cannot afford this item!");
+							return;
+						}
+						GameBlade.api.getStarsHandler().takeStars(player, (int)(Math.ceil(c.getPrice())));
+						player.sendMessage(ChatColor.RED+"-"+(int)(Math.ceil(c.getPrice()))+ChatColor.YELLOW+" stars");
+					}
+					else if(currency.equals(Currency.TOKENS)){
+						int bal;
+						try {
+							bal = GameBlade.api.getTokenHandler().getTokens(player);
+						} catch (TokenServiceUnavailableException e) {
+							//Oh poo
+							player.sendMessage(ChatColor.RED+"This server has lost connection to tokens! Please try on another!");
+							return;
+						}
+						if(bal < c.getPrice()){
+							player.sendMessage(ChatColor.RED+"You cannot afford this item!");
+							return;
+						}
+						GameBlade.api.getTokenHandler().takeTokens(player, (int)(Math.ceil(c.getPrice())));
+						player.sendMessage(ChatColor.RED+"-"+(int)(Math.ceil(c.getPrice()))+ChatColor.YELLOW+" tokens");
+					}
+				}
+				//We've taken their money, now to give them the stuff
+				
+				//Give the item
+				awardCosmetic(player, c, owned);
+				
+				Bukkit.getScheduler().runTask(GameBlade.plugin, new Runnable(){
+
+					@Override
+					public void run() {
+						postPurchase(player, c);
+						return;
+					}});
+				return;
+			}});
+	}
+	
+	public void awardCosmetic(Player player, Cosmetic c, List<String> owned){
+		notSync();
+		owned.add(c.getID());
+		StringBuilder sb = new StringBuilder();
+		for(String id:owned){
+			if(sb.length() < 1){
+				sb.append(id);
+				continue;
+			}
+			sb.append("|").append(id);
+		}
+		
+		try {
+			GameBlade.plugin.GBSQL.setInTable(SQL_TABLE, SQL_ID_KEY, PlayerIDFinder.getMojangID(player).getID(), SQL_COSMETICS_KEY, sb.toString());
+		} catch (SQLException e) {
+			// Oh crap
+			e.printStackTrace();
+			if(c.getCurrencyUsed().equals(Currency.TOKENS)){
+				GameBlade.api.getTokenHandler().awardTokens(player, (int)(Math.ceil(c.getPrice())));
+			}
+			else if(c.getCurrencyUsed().equals(Currency.STARS)){
+				GameBlade.api.getStarsHandler().awardStars(player, (int)(Math.ceil(c.getPrice())));
+			}
+			player.sendMessage(ChatColor.RED+"Awfully sorry, your transaction failed to complete. But we've refunded the cost.");
+			player.sendMessage(ChatColor.GREEN+"Refunded!");
+			return;
+		}
+	}
+	
+	public void postPurchase(Player player, Cosmetic c){
+		player.sendMessage(ChatColor.GREEN+"Successfully purchased item!");
+		//TODO ?
 	}
 	
 	public List<String> getOwnedCosmeticIds(Player player){
@@ -84,7 +171,13 @@ public class CosmeticManager {
 		String owned;
 		
 		try {
-			owned = GameBlade.plugin.GBSQL.searchTable(SQL_TABLE, SQL_ID_KEY, uuid, SQL_COSMETICS_KEY).toString();
+			Object o = GameBlade.plugin.GBSQL.searchTable(SQL_TABLE, SQL_ID_KEY, uuid, SQL_COSMETICS_KEY);
+			if(o != null){
+				owned = o.toString();
+			}
+			else {
+				owned = null;
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return new ArrayList<String>();
