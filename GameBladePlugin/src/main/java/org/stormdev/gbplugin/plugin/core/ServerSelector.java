068cbb259655f55288a5e1308ebbb21fb1e0f8ee
storm345 <storm345dev@gmail.com>
2015-01-19 21:40:46 +0000

    Re-code server selector to be sexy

--- GameBladePlugin/src/main/java/org/stormdev/gbplugin/plugin/core/ServerSelector.java
+++ GameBladePlugin/src/main/java/org/stormdev/gbplugin/plugin/core/ServerSelector.java
@@ -1,15 +1,12 @@
 package org.stormdev.gbplugin.plugin.core;
 
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -19,25 +16,408 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.stormdev.gbapi.core.APIProvider;
 import org.stormdev.gbapi.gui.IconMenu;
 import org.stormdev.gbapi.gui.IconMenu.OptionClickEvent;
-import org.stormdev.gbapi.storm.skulls.CustomPlayerHeads;
+import org.stormdev.gbapi.gui.IconMenu.OptionClickEventHandler;
 import org.stormdev.gbplugin.plugin.server.ping.ServerListPing;
 import org.stormdev.gbplugin.plugin.server.ping.ServerListPing.StatusResponse;
 import org.stormdev.gbplugin.plugin.utils.Colors;
 import org.stormdev.servermanager.api.messaging.Server;
 
-public class ServerSelector implements CommandExecutor, Listener
+public class ServerSelector implements CommandExecutor, Listener, OptionClickEventHandler
 {
 	private static final String INVENTORY_TITLE = ChatColor.RED + "Choose a server";
-    public String data = null;
+	
+	private static IconMenu menu = null;
+	
+	private static GameBlade plugin;
+	
+	private static List<SelectableServer> rootServers = new ArrayList<SelectableServer>();
+	
+	public static interface SelectableServer {
+		public String getDisplayName();
+		public List<String> getLore();
+		public void onSelect(OptionClickEvent event, Player player);
+		public void update(IconMenu menu);
+		public boolean isChild();
+		public MultipleServer getParent();
+		public int getSlot();
+	}
+	
+	private static class MultipleServer implements SelectableServer, OptionClickEventHandler {
+		private String name;
+		private List<String> lore;
+		/*private String bungeePrefix; //eg. 'mk' matches, 'mk1' and 'mk2'
+*/		private String mineManagerPrefix; //Eg. 'GB MarioKart' matches 'GB MarioKart 1', etc...
+		/*private String ip = "localhost"; //Eg. 'alpha.gameblade.net' and 'bravo.gameblade.net'
+		private int minPort;
+		private int maxPort;*/
+		private int online;
+		private int maxPlayers;
+		private List<SelectableServer> subServers = new ArrayList<SelectableServer>();
+		private IconMenu subMenu;
+		private ItemStack icon;
+		private int slot;
+		
+		public MultipleServer(ItemStack icon, int slot, int invSize, String displayName,/* String bungeePrefix,*/ String mineManagerPrefix, String... lore){
+			this.icon = icon;
+			this.slot = slot;
+			this.name = displayName;
+			/*this.bungeePrefix = bungeePrefix;*/
+			this.mineManagerPrefix = mineManagerPrefix;
+			this.lore = new ArrayList<String>(Arrays.asList(lore));
+			
+			this.subMenu = new IconMenu(name, invSize, this, plugin);
+			
+			rootServers.add(this);
+			
+			task();
+			callUpdate(this);
+		}
+		
+		private void task(){
+			Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, new Runnable(){
+
+				@Override
+				public void run() {
+				        int online = 0;
+				        int max = 0;
+				        if(GameBlade.smApi != null){
+				        	//ServerID = "GB MTASA 1";
+				        	Map<String, Server> srvs = GameBlade.smApi.getServers().getServers();
+				        	if(srvs != null && srvs.size() > 0){
+				        		for(String s:new ArrayList<String>(srvs.keySet())){
+				        			if(s.trim().toLowerCase().startsWith(mineManagerPrefix.trim().toLowerCase())){
+				        				Server se = srvs.get(s);
+				        				if(se != null){
+				        					online += se.getOnlinePlayerCount();
+				        					max += se.getMaxPlayers();
+				        				}
+				        			}
+				        		}
+				        	}
+				        }
+				        MultipleServer.this.maxPlayers = max;
+				        MultipleServer.this.online = online;
+				        Bukkit.getScheduler().runTask(plugin, new Runnable(){
+
+							@Override
+							public void run() {
+								callUpdate(MultipleServer.this);
+								return;
+							}});
+					return;
+				}}, 0l, 10*20l);
+		}
+		
+		@Override
+		public String getDisplayName() {
+			return name;
+		}
+
+		@Override
+		public List<String> getLore() {
+			List<String> lore = new ArrayList<String>();
+			lore.addAll(this.lore);
+			lore.add(ChatColor.GRAY + "" + ChatColor.STRIKETHROUGH + "------------------------------------");
+			lore.add(ChatColor.WHITE+""+online+"/"+maxPlayers);
+			return lore;
+		}
+
+		@Override
+		public void onSelect(OptionClickEvent event, final Player player) {
+			player.sendMessage(ChatColor.GRAY+"Opening...");
+			player.closeInventory();
+			
+			Bukkit.getScheduler().runTaskLater(plugin, new Runnable(){
+
+				@Override
+				public void run() {
+					subMenu.open(player);
+					return;
+				}}, 2l);
+		}
+
+		@Override
+		public void update(IconMenu menu) {
+			menu.setOption(slot, icon.clone(), getDisplayName(), getLore());
+		}
+
+		@Override
+		public boolean isChild() {
+			return false;
+		}
+
+		@Override
+		public MultipleServer getParent() {
+			return null;
+		}
+
+		@Override
+		public int getSlot() {
+			return slot;
+		}
+
+		@Override
+		public void onOptionClick(OptionClickEvent event) {
+			event.setWillClose(true);
+			event.setWillDestroy(false);
+			
+			int slot = event.getPosition();
+			for(SelectableServer ss:subServers){
+				if(ss.getSlot() == slot){
+					ss.onSelect(event, event.getPlayer());
+				}
+			}
+		}
+		
+	}
+	
+	private static class SingleServer implements SelectableServer {
+		private String displayName; //Eg. Survival
+		private List<String> lore;
+		private String bungeeName; //Eg. survival would match survival1, survival2, etc...
+		private String mineManagerNamePrefix; //Eg. GB Survival would match GB Survival 1, etc...
+		private int maxPlayers = 50;
+		private int online = 0;
+		private int slot;
+		private ItemStack icon;
+		private boolean child = false;
+		private MultipleServer parent = null;
+		private boolean isOnline;
+		
+		private String ip = null;
+		private int port = -1;
+		private String MOTD = null;
+		
+		public SingleServer(ItemStack icon, int slot, String displayName, String bungeeName, String mineManagerPrefix, String... lore){
+			this(icon, slot, displayName, bungeeName, mineManagerPrefix, null, lore);
+		}
+		
+		public SingleServer(ItemStack icon, int slot, String displayName, String bungeeName, String mineManagerPrefix, MultipleServer parent, String... lore){
+			this.icon = icon;
+			this.slot = slot;
+			this.displayName = displayName;
+			this.bungeeName = bungeeName;
+			this.mineManagerNamePrefix = mineManagerPrefix;
+			this.lore = new ArrayList<String>(Arrays.asList(lore));
+			
+			if(parent != null){
+				this.child = true;
+				this.parent = parent;
+				parent.subServers.add(this);
+			}
+			else {
+				rootServers.add(this);
+			}
+			
+			task();
+			callUpdate(this);
+		}
+		
+		public void setUseMOTD(String ip, int port){
+			this.ip = ip;
+			this.port = port;
+		}
+		
+		private void task(){
+			Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, new Runnable(){
+
+				@Override
+				public void run() {
+				        int online = 0;
+				        int max = 0;
+				        boolean isOnline = false;
+				        if(GameBlade.smApi != null){
+				        	//ServerID = "GB MTASA 1";
+				        	Map<String, Server> srvs = GameBlade.smApi.getServers().getServers();
+				        	if(srvs != null && srvs.size() > 0){
+				        		for(String s:new ArrayList<String>(srvs.keySet())){
+				        			if(s.trim().toLowerCase().startsWith(mineManagerNamePrefix.trim().toLowerCase())){
+				        				Server se = srvs.get(s);
+				        				if(se != null){
+				        					online += se.getOnlinePlayerCount();
+				        					max += se.getMaxPlayers();
+				        					isOnline = true;
+				        				}
+				        			}
+				        		}
+				        	}
+				        }
+				        SingleServer.this.isOnline = isOnline;
+				        SingleServer.this.maxPlayers = max;
+				        SingleServer.this.online = online;
+				        Bukkit.getScheduler().runTask(plugin, new Runnable(){
+
+							@Override
+							public void run() {
+								callUpdate(SingleServer.this);
+								return;
+							}});
+				        
+				        if(ip != null && port > -1){
+				        	StatusResponse resp;
+							try {
+								resp = ServerListPing.fetchData(new InetSocketAddress(ip, port));
+							} catch (IOException e) {
+								return;
+							}
+							if(resp == null){
+								return;
+							}
+							MOTD = resp.getDescription();
+				        }
+					return;
+				}}, 0l, 10*20l);
+		}
+		
+		public int getSlot(){
+			return this.slot;
+		}
+		
+		@Override
+		public String getDisplayName() {
+			return displayName;
+		}
+		@Override
+		public List<String> getLore() {
+			List<String> lore = new ArrayList<String>();
+			lore.addAll(this.lore);
+			lore.add(ChatColor.GRAY + "" + ChatColor.STRIKETHROUGH + "------------------------------------");
+			if(MOTD != null){
+				lore.add(Colors.colorise(MOTD));
+			}
+			if(isOnline){
+				lore.add(ChatColor.WHITE+""+online+"/"+maxPlayers);
+			}
+			else {
+				lore.add(ChatColor.RED+"Restarting...");
+			}
+			return lore;
+		}
+		@Override
+		public void onSelect(OptionClickEvent event, Player player) {
+			player.sendMessage(ChatColor.GRAY+"Connecting to "+getDisplayName()+ChatColor.RESET+""+ChatColor.GRAY+"...");
+			PlayerServerSender.sendToServer(player, bungeeName);
+		}
+
+		@Override
+		public void update(IconMenu menu) {
+			menu.setOption(slot, icon.clone(), getDisplayName(), getLore());
+		}
+
+		@Override
+		public boolean isChild() {
+			return child;
+		}
+
+		@Override
+		public MultipleServer getParent() {
+			return this.parent;
+		}
+	}
+	
+	public ServerSelector(GameBlade plugin){
+		ServerSelector.plugin = plugin;
+		Bukkit.getPluginManager().registerEvents(this, plugin);
+		initMenu();
+	}
+	
+	private static void callUpdate(SelectableServer server){
+		if(!server.isChild()){
+			//It's on the main menu
+			server.update(menu);
+		}
+		else if(server.getParent() != null){ //It's on a sub-menu
+			server.update(server.getParent().subMenu);
+		}
+	}
+	
+	private void initMenu(){
+		menu = new IconMenu(INVENTORY_TITLE, 18, this, plugin);
+		
+		SingleServer lobby = new SingleServer(new ItemStack(Material.BOOKSHELF), 0, 
+				ChatColor.RED+"Main Lobby", 
+				"hub1", "GB Lobby", 
+				ChatColor.GRAY+""+ChatColor.ITALIC+"Go back to the main lobby");
+		SingleServer mtasa = new SingleServer(new ItemStack(Material.IRON_SWORD), 2, 
+				ChatColor.RED+"MineTheftAuto", 
+				"mtasa", "GB MTASA", 
+				ChatColor.GOLD+""+ChatColor.ITALIC+"Grand Theft Auto in Minecraft!");
+		MultipleServer mk = new MultipleServer(new ItemStack(Material.MINECART), 3, 9,
+				ChatColor.RED+"MarioKart", 
+				"GB MarioKart", 
+				ChatColor.GOLD+""+ChatColor.ITALIC+"MarioKart recreated in Minecraft!");
+		SingleServer mk1 = new SingleServer(new ItemStack(Material.MINECART), 0, ChatColor.RED+"MK 1", "mk1", "GB MarioKart 1", mk, 
+				ChatColor.GOLD+"Join MarioKart 1");
+		mk1.setUseMOTD("bravo.gameblade.net", 4011);
+		SingleServer mk2 = new SingleServer(new ItemStack(Material.MINECART), 1, ChatColor.RED+"MK 2", "mk2", "GB MarioKart 2", mk, 
+				ChatColor.GOLD+"Join MarioKart 2");
+		mk2.setUseMOTD("bravo.gameblade.net", 4012);
+		SingleServer plots = new SingleServer(new ItemStack(Material.GRASS), 4, 
+				ChatColor.GREEN+"Creative Plots", 
+				"plots", "GB Plots", 
+				ChatColor.AQUA+""+ChatColor.ITALIC+"Build what you want in your own plot!");
+		SingleServer me = new SingleServer(new ItemStack(Material.IRON_BOOTS), 5, 
+				ChatColor.RED+"Mirrors"+ChatColor.WHITE+" Edge", 
+				"mirrorsedge1", "GB MirrorsEdge", 
+				ChatColor.AQUA+""+ChatColor.ITALIC+"The famous parkour game meets",
+				ChatColor.AQUA+""+ChatColor.ITALIC+"Minecraft, including a storyline!");
+		SingleServer survival = new SingleServer(new ItemStack(Material.DIAMOND_SWORD), 6, 
+				ChatColor.GOLD+"Survival", 
+				"survival1", "GB Survival", 
+				ChatColor.AQUA+""+ChatColor.ITALIC+"Survival Minecraft",
+				ChatColor.AQUA+""+ChatColor.ITALIC+"Build, explore and battle with friends!");
+		
+		SingleServer events = new SingleServer(new ItemStack(Material.NETHER_STAR), 8, 
+				ChatColor.GOLD+"Event Server", 
+				"eventServer", "GB Event", 
+				ChatColor.RED+""+ChatColor.ITALIC+"Coming soon!");
+	}
+	
+	public void open(final Player p){
+		p.closeInventory();
+		Bukkit.getScheduler().runTaskLater(plugin, new Runnable(){
+
+			@Override
+			public void run() {
+				menu.open(p);
+		        p.playSound(p.getLocation(), Sound.CLICK, 1.0F, 10.0F);
+		        return;
+			}}, 2l);
+    }
+	
+	@Override
+    public boolean onCommand(CommandSender sender, Command cmd, String alias,
+                             String[] args) {
+        if(!(sender instanceof Player)){
+        	sender.sendMessage(ChatColor.RED+"Players only!");
+        	return true;
+        }
+        
+        Player player = ((Player)sender);
+        open(player);
+        return true;
+    }
+	
+
+	@Override
+	public void onOptionClick(OptionClickEvent event) {
+		event.setWillClose(true);
+		event.setWillDestroy(false);
+		
+		int slot = event.getPosition();
+		for(SelectableServer ss:rootServers){
+			if(ss.getSlot() == slot){
+				ss.onSelect(event, event.getPlayer());
+			}
+		}
+		
+		return;
+	}
+    /*public String data = null;
     static GameBlade plugin;
     public static Inventory serverSelector = Bukkit.createInventory(null, 18, INVENTORY_TITLE);
     private ItemMeta immta;
@@ -181,7 +561,7 @@ public class ServerSelector implements CommandExecutor, Listener
 			        mtalore.add(ChatColor.WHITE + "" + online + ChatColor.BOLD + "/" + ChatColor.WHITE + "150");
 			        immta.setLore(mtalore);
 			        mta.setItemMeta(immta);
-			        serverSelector.setItem(2, mta);
+			        serverSelector.setItem(1, mta);
 				return;
 			}}, 10*20l, 10*20l);
         immta.setLore(mtalore);
@@ -228,7 +608,7 @@ public class ServerSelector implements CommandExecutor, Listener
 			        plotslore.add(ChatColor.WHITE + "" + online + ChatColor.BOLD + "/" + ChatColor.WHITE + "100");
 			        implots.setLore(plotslore);
 			        plots.setItemMeta(implots);
-			        serverSelector.setItem(4, plots);
+			        serverSelector.setItem(3, plots);
 				return;
 			}}, 10*20l, 10*20l);
         implots.setLore(plotslore);
@@ -268,7 +648,7 @@ public class ServerSelector implements CommandExecutor, Listener
 			        slore.add(ChatColor.WHITE + "" + online + ChatColor.BOLD + "/" + ChatColor.WHITE + "100");
 			        imsurvival.setLore(slore);
 			        survival.setItemMeta(imsurvival);
-			        serverSelector.setItem(6, survival);
+			        serverSelector.setItem(5, survival);
 				return;
 			}}, 10*20l, 10*20l);
         imsurvival.setLore(survivallore);
@@ -315,7 +695,7 @@ public class ServerSelector implements CommandExecutor, Listener
 			        melore.add(ChatColor.WHITE + "" + online + ChatColor.BOLD + "/" + ChatColor.WHITE + "50");
 			        imme.setLore(melore);
 			        mirrorsEdge.setItemMeta(imme);
-			        serverSelector.setItem(5, mirrorsEdge);
+			        serverSelector.setItem(4, mirrorsEdge);
 				return;
 			}}, 10*20l, 10*20l);
         imme.setLore(melore);
@@ -355,7 +735,7 @@ public class ServerSelector implements CommandExecutor, Listener
 			        mklore.add(ChatColor.WHITE + "" + online + ChatColor.BOLD + "/" + ChatColor.WHITE + "50");
 			        immk.setLore(mklore);
 			        mk.setItemMeta(immk);
-			        serverSelector.setItem(3, mk);
+			        serverSelector.setItem(2, mk);
 				return;
 			}}, 10*20l, 10*20l);
         immk.setLore(mklore);
@@ -381,18 +761,22 @@ public class ServerSelector implements CommandExecutor, Listener
         GBSite.setItemMeta(im);
         
         serverSelector.setItem(8, vipservers);
-        serverSelector.setItem(2, mta);
-        serverSelector.setItem(3, mk);
-        serverSelector.setItem(4, plots);
-        serverSelector.setItem(5, mirrorsEdge);
-        serverSelector.setItem(6, survival);
+        serverSelector.setItem(1, mta);
+        serverSelector.setItem(2, mk);
+        serverSelector.setItem(3, plots);
+        serverSelector.setItem(4, mirrorsEdge);
+        serverSelector.setItem(5, survival);
+        serverSelector.setItem(6, collectTheWool);
+        serverSelector.setItem(7, wip);
         serverSelector.setItem(0, lobby);
 
+        serverSelector.setItem(10, wip);
         serverSelector.setItem(11, wip);
         serverSelector.setItem(12, wip);
         serverSelector.setItem(13, wip);
         serverSelector.setItem(14, wip);
         serverSelector.setItem(15, wip);
+        serverSelector.setItem(16, wip);
     }
     
     public void open(Player p){
@@ -520,5 +904,5 @@ public class ServerSelector implements CommandExecutor, Listener
         Player player = ((Player)sender);
         open(player);
         return true;
-    }
+    }*/
 }
